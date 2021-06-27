0eb27fba4f150f10b4b3aebfa08be8d4552d0209
d-delaey <svenschroeder1998@gmail.com>
2021-06-27 17:52:03 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+lang-files-translator
+=====================
+
+
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/lang-files-translator.svg)](https://npmjs.org/package/lang-files-translator)
+[![Downloads/week](https://img.shields.io/npm/dw/lang-files-translator.svg)](https://npmjs.org/package/lang-files-translator)
+[![License](https://img.shields.io/npm/l/lang-files-translator.svg)](https://github.com/d-delaey/lang-files-translator/blob/master/package.json)
+
+<!-- toc -->
+* [Usage](#usage)
+* [Commands](#commands)
+<!-- tocstop -->
+# Usage
+<!-- usage -->
+```sh-session
+$ npm install -g lang-files-translator
+$ lang-files-translator COMMAND
+running command...
+$ lang-files-translator (-v|--version|version)
+lang-files-translator/0.0.1 linux-x64 node-v16.3.0
+$ lang-files-translator --help [COMMAND]
+USAGE
+  $ lang-files-translator COMMAND
+...
+```
+<!-- usagestop -->
+# Commands
+<!-- commands -->
+
+<!-- commandsstop -->
---
+++ src/index.js
@@ -0,0 +1,57 @@
+const {Command, flags} = require("@oclif/command");
+const parser = require("gettext-parser");
+const fs = require("fs");
+const translate = require("@vitalets/google-translate-api");
+
+class LangFilesTranslatorCommand extends Command {
+	static args = [
+		{
+			name: "file",
+			description: "Language File (.pot) that will be translated",
+			required: true,
+		},
+		{name: "output", description: "Output File Name", required: true},
+		{
+			name: "lang",
+			description: "Language to be translated into",
+			required: true,
+		},
+	];
+
+	async run() {
+		const {args} = this.parse(LangFilesTranslatorCommand);
+
+		let file = fs.readFileSync(args.file);
+		let defaultTranslations = parser.po.parse(file);
+
+		for (let [key, value] of Object.entries(defaultTranslations.translations[""])) {
+			if (!value.msgid) {
+				continue;
+			}
+
+			try {
+				let translation = await translate(value.msgid, {to: args.lang});
+				value.msgstr = translation.text;
+			} catch (e) {
+				console.error("String: " + value.msgid + " could not be translated.");
+				console.error(e);
+				continue;
+			}
+		}
+
+		let poOutput = parser.po.compile(defaultTranslations);
+		fs.writeFileSync(args.output + ".po", poOutput);
+
+		let moOutput = parser.mo.compile(defaultTranslations);
+		fs.writeFileSync(args.output + ".mosf", moOutput);
+	}
+}
+
+LangFilesTranslatorCommand.description = `CLI Tool to Translate .pot or .po Files automaticly with Google Translate`;
+
+LangFilesTranslatorCommand.flags = {
+	version: flags.version({char: "v"}),
+	help: flags.help({char: "h"}),
+};
+
+module.exports = LangFilesTranslatorCommand;
